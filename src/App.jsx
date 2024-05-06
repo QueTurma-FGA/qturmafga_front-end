611e74247a0088be1d9d7116aa130c7d4831611a
LC_ <lclaudio.tl@gmail.com>
2024-05-06 20:23:39 -0300

    Creating home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
+import './global.css'
+
 const App = () => {
   return (
     <BrowserRouter >
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,20 @@
+import logo from '../../assets/logo.png'
+import fundoAprender from '../../assets/fundoAprender2.png'
+import styles from './home.module.css'
+
 const Home = () => {
+  
   return (
-      <h1>Home</h1>
+    <div className={styles.homeContainer}>
+      <img src={logo} alt="Logo UnB Qturma?FGA" />
+      <img src={fundoAprender} alt="Logo UnB Qturma?FGA" />
+
+      <span>Por favor, insira o código da disciplina UnB que deseja consultar</span>
+
+      <footer>
+        <p>&copy; Feudo Preto - Terras Devastadas</p>
+      </footer>
+    </div>
   );
 }
 
